abdf36944176e82267ba2235c712caba194f779a
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-12-23 18:04:53 +0900

    feat: sonner Toaster 적용

---
+++ src/components/overlay/Toaster.tsx
@@ -0,0 +1,46 @@
+import {
+  CircleCheckIcon,
+  InfoIcon,
+  Loader2Icon,
+  OctagonXIcon,
+  TriangleAlertIcon,
+} from "lucide-react"
+import { Toaster as Sonner, type ToasterProps } from "sonner"
+
+/**
+ * 
+ * @example
+ * onClick={() => {
+ *   toast.success("Success", {
+ *     action: {
+ *       label: "Undo",
+ *       onClick: () => console.log("Undo"),
+ *     },
+ *   })
+ * }}
+ */
+const Toaster = ({ ...props }: ToasterProps) => {
+  return (
+    <Sonner
+      className="toaster group"
+      icons={{
+        success: <CircleCheckIcon className="size-4" />,
+        info: <InfoIcon className="size-4" />,
+        warning: <TriangleAlertIcon className="size-4" />,
+        error: <OctagonXIcon className="size-4" />,
+        loading: <Loader2Icon className="size-4 animate-spin" />,
+      }}
+      style={
+        {
+          "--normal-bg": "var(--popover)",
+          "--normal-text": "var(--popover-foreground)",
+          "--normal-border": "var(--border)",
+          "--border-radius": "var(--radius)",
+        } as React.CSSProperties
+      }
+      {...props}
+    />
+  )
+}
+
+export { Toaster }
