f8652a50ebe82a4e2f2b169af08cca280e1401fe
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-12-24 12:42:54 +0900

    fix: 로그인 스키마 수정

--- src/schema/auth/login.schema.ts
+++ src/schema/auth/login.schema.ts
@@ -1,9 +1,12 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 const loginSchema = z.object({
-	email: z.email('이메일과 비밀번호를 다시 확인해주세요'),
-	password: z.string().min(1, '이메일과 비밀번호를 다시 확인해주세요')
-})
+  email: z
+    .string()
+    .min(1, "이메일과 비밀번호를 다시 확인해주세요")
+    .includes("@", { message: "올바른 이메일 형식이 아닙니다" }),
+  password: z.string().min(1, "이메일과 비밀번호를 다시 확인해주세요"),
+});
 
 type LoginSchema = z.infer<typeof loginSchema>;
 
