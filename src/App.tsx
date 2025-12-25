9eb8f7ed3521f0798f8423c091e8a42d2fb2b0fc
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-12-25 23:15:32 +0900

    feat: 회원가입 페이지 UI 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-const LoginPage = lazy(() => import("@/pages/login/LoginPage"));
+const LoginPage = lazy(() => import("@/pages/auth/login/LoginPage"));
+const SignupPage = lazy(() => import("@/pages/auth/signup/SignupPage"));
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="h-dvh w-dvw bg-white">
         <Routes>
           <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<SignupPage />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ src/pages/auth/AuthLayout.tsx
@@ -0,0 +1,36 @@
+import type { ReactNode } from "react";
+import loginBg from "@/assets/images/login-bg.png";
+import logoTextHorizontal from "@/assets/images/logo-text-horizontal.svg";
+
+interface AuthLayoutProps {
+  children: ReactNode;
+}
+
+function AuthLayout({ children }: Readonly<AuthLayoutProps>) {
+  return (
+    <div className="relative flex min-h-dvh w-dvw md:items-start md:pl-6 lg:items-center lg:pl-15">
+      <div className="center min-h-dvh flex-col gap-10 overflow-y-auto px-5 md:flex-[0.4] md:py-8 lg:gap-12 lg:py-16">
+        <img
+          src={logoTextHorizontal}
+          alt="logo text horizontal"
+          className="w-40 max-w-50 md:w-[37%]"
+        />
+        {children}
+      </div>
+      <div className="top-0 hidden h-screen md:sticky md:block md:flex-[0.6] lg:relative">
+        <div className="relative h-full md:p-4 lg:p-6">
+          <img
+            src={loginBg}
+            alt="login background"
+            loading="eager"
+            fetchPriority="high"
+            aria-label="login background"
+            className="h-full w-full object-cover md:rounded-3xl"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AuthLayout;
