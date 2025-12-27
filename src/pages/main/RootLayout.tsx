68f6aa4e772ec63e8bdeb5a3db8b956a631e3670
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-12-27 16:12:14 +0900

    style: state 이름 변경 isLoggedIn -> isUser

--- src/pages/main/RootLayout.tsx
+++ src/pages/main/RootLayout.tsx
@@ -5,14 +5,14 @@ import MobileSidebar from "@/components/layout/MobileSidebar";
 import Sidebar from "@/components/layout/Sidebar";
 
 function RootLayout() {
-  const [isLoggedIn] = useState(false);
+  const [isUser] = useState(false);
 
   // TODO: 로그인 여부 확인 후 리다이렉트
 
   return (
     <MobileSidebar>
       {/* 승인된 매장이 있을 경우 */}
-      {isLoggedIn ? (
+      {isUser ? (
         <div className="flex h-dvh w-dvw flex-col bg-gray-700 md:flex-row md:gap-2.5 md:px-5 md:py-5 lg:gap-6 lg:px-15 lg:py-8">
           <aside className="hidden h-auto flex-col rounded-[28px] bg-white md:flex md:w-[186px] md:gap-3 lg:w-[318px] lg:gap-4">
             <Sidebar />
