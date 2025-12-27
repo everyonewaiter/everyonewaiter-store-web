254427f1075f4f34f3ad6a62e709bb5e19d23470
handwoong <handwoong@gmail.com>
2025-12-27 18:56:15 +0900

    comment: 주석 위치 변경

--- src/pages/main/RootLayout.tsx
+++ src/pages/main/RootLayout.tsx
@@ -11,8 +11,8 @@ function RootLayout() {
 
   return (
     <MobileSidebar>
-      {/* 승인된 매장이 있을 경우 */}
       {isUser ? (
+        // 승인된 매장이 있을 경우
         <div className="flex h-dvh w-dvw flex-col bg-gray-700 md:flex-row md:gap-2.5 md:px-5 md:py-5 lg:gap-6 lg:px-15 lg:py-8">
           <aside className="hidden h-auto flex-col rounded-[28px] bg-white md:flex md:w-46.5 md:gap-3 lg:w-79.5 lg:gap-4">
             <Sidebar />
