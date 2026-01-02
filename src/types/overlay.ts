6ce7f4b71a09d745dcf0df19edda98be70e7dd3f
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2026-01-02 14:27:43 +0900

    feat: overlay type에 AlertProps 작성

--- src/types/overlay.ts
+++ src/types/overlay.ts
@@ -2,3 +2,9 @@ export interface ModalProps {
   isOpen: boolean;
   close: () => void;
 }
+
+export interface AlertProps {
+  close: () => void;
+  onConfirm: () => void;
+  onCancel: () => void;
+}
