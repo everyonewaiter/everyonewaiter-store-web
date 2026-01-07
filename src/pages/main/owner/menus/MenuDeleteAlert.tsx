c00fb22f3b9eaac293dc4b10a04eeec5e6b1f8a5
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2026-01-07 13:09:51 +0900

    fix: MenuDeleteAlert 모바일 스크린 css 추가

--- src/pages/main/owner/menus/MenuDeleteAlert.tsx
+++ src/pages/main/owner/menus/MenuDeleteAlert.tsx
@@ -7,16 +7,18 @@ function MenuDeleteAlert({ close }: Readonly<Pick<AlertProps, "close">>) {
   };
 
   return (
-    <Alert onClose={close}>
-      <Alert.Header className="h-25">
-        <Alert.Title className="text-xl!">선택한 메뉴를 삭제하시겠습니까?</Alert.Title>
+    <Alert onClose={close} layoutClassName="w-[calc(100%-40px)]">
+      <Alert.Header className="h-14 md:h-25">
+        <Alert.Title className="text-base! md:text-xl!">
+          선택한 메뉴를 삭제하시겠습니까?
+        </Alert.Title>
       </Alert.Header>
 
       <Alert.Footer>
-        <Alert.Cancel color="grey" onClick={close} className="button-lg">
+        <Alert.Cancel color="grey" onClick={close} className="button-sm md:button-lg flex-[0.4]">
           닫기
         </Alert.Cancel>
-        <Alert.Action color="primary" onClick={handleConfirm} className="button-lg">
+        <Alert.Action color="primary" onClick={handleConfirm} className="button-sm md:button-lg">
           삭제
         </Alert.Action>
       </Alert.Footer>
