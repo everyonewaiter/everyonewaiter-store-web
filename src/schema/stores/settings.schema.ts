353b17fb639d0f3cc000c9c9a5ae9d676d104ff6
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2026-01-10 23:18:35 +0900

    fix: ksnetDeviceNo maxLength 수정 (10 -> 20)

--- src/schema/stores/settings.schema.ts
+++ src/schema/stores/settings.schema.ts
@@ -4,16 +4,19 @@ const settingsSchema = z.object({
   ksnetDeviceNo: z
     .string()
     .min(1, "기기 번호를 입력해주세요.")
-		.max(10, "기기 번호는 10자 이내로 입력해주세요."),
-	printerLocation: z.enum(["POS", "HALL"]),
-	showMenuPopup: z.boolean(),
-	showOrderTotalPrice: z.boolean(),
-	showOrderMenuImage: z.boolean(),
-	countryOfOrigins: z.array(z.object({
-		item: z.string(),
-		origin: z.string(),
-	})),
-	staffCallOptions: z.array(z.string()),
+    .max(20, "기기 번호는 20자 이내로 입력해주세요."),
+  printerLocation: z.enum(["POS", "HALL"]),
+  showMenuPopup: z.boolean(),
+  showOrderTotalPrice: z.boolean(),
+  showOrderMenuImage: z.boolean(),
+  countryOfOrigins: z.array(
+    z.object({
+      item: z.string(),
+      origin: z.string(),
+    })
+  ),
+  staffCallOptions: z.array(z.string()),
+  extraTableCount: z.number().min(0, "추가 테이블 수는 0 이상이어야 합니다."),
 });
 
 type SettingsSchema = z.infer<typeof settingsSchema>;
