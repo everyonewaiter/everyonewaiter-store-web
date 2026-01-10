21351723c8e1ec1ae8f238cb9754cb0a34c3c8af
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2026-01-10 23:19:06 +0900

    refactor: 설정 페이지 리팩토링 및 컴포넌트 분리

--- src/pages/main/owner/settings/MainSettingsPage.tsx
+++ src/pages/main/owner/settings/MainSettingsPage.tsx
@@ -1,16 +1,15 @@
 import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { overlay } from "overlay-kit";
 import { useForm, useWatch } from "react-hook-form";
 import { Form, FormErrorMessage } from "@/components/form/Form";
 import { Info, Plus } from "@/components/icons";
 import Button from "@/components/ui/Button/Button";
 import Input from "@/components/ui/Input";
-import Switch from "@/components/ui/Switch";
+import Label from "@/components/ui/Label";
 import cn from "@/lib/utils";
-import SettingsDeviceNumberAlert from "@/pages/main/owner/settings/SettingsDeviceNumberAlert";
 import SettingsSection from "@/pages/main/owner/settings/SettingsSection";
 import SettingsStaffCallChip from "@/pages/main/owner/settings/SettingsStaffCallChip";
+import SettingsSwitchItem from "@/pages/main/owner/settings/SettingsSwitchItem";
 import { settingsSchema, type SettingsSchema } from "@/schema/stores/settings.schema";
 import type { PrinterLocation } from "@/types/domain/store";
 
@@ -20,44 +19,43 @@ function MainSettingsPage() {
     mode: "onSubmit",
     reValidateMode: "onChange",
     defaultValues: {
-      ksnetDeviceNo: "",
+      ksnetDeviceNo: "DPTOTEST03",
       printerLocation: "POS",
       showMenuPopup: false,
       showOrderTotalPrice: false,
       showOrderMenuImage: false,
       staffCallOptions: ["반찬 추가", "음료 추가", "직원 호출"],
+      extraTableCount: 5,
     },
   });
 
   const [newStaffCallOption, setNewStaffCallOption] = useState<string>("");
 
+  const printerLocation = useWatch({ control: form.control, name: "printerLocation" });
+  const deviceNo = useWatch({ control: form.control, name: "ksnetDeviceNo" });
+  const staffCallOptions = useWatch({ control: form.control, name: "staffCallOptions" });
+
   const handleAddStaffCallOption = () => {
     // TODO: 직원 호출 옵션 추가 로직
   };
-
-  const printerLocation = useWatch({ control: form.control, name: "printerLocation" });
-  const deviceNo = useWatch({ control: form.control, name: "ksnetDeviceNo" });
-  const showMenuPopup = useWatch({ control: form.control, name: "showMenuPopup" });
-  const showOrderTotalPrice = useWatch({ control: form.control, name: "showOrderTotalPrice" });
-  const showOrderMenuImage = useWatch({ control: form.control, name: "showOrderMenuImage" });
-  const staffCallOptions = useWatch({ control: form.control, name: "staffCallOptions" });
 
   const handleChangePrinterLocation = (location: PrinterLocation) => {
     // TODO: 프린터 위치 변경 로직
     form.setValue("printerLocation", location);
   };
 
-  const handleUpdateDeviceNumber = () => {
-    if (deviceNo.startsWith("DPTOTEST")) {
-      overlay.open((overlayProps) => (
-        <SettingsDeviceNumberAlert {...overlayProps} deviceNumber={deviceNo} />
-      ));
-    }
+  const handleRegisterDeviceNumber = () => {
+    // TODO: 기기 번호 등록 로직
   };
 
   return (
     <Form {...form}>
-      <div className="flex h-full w-full flex-col items-center bg-white lg:justify-center">
+      <div
+        className={cn(
+          "flex h-full w-full flex-col items-center bg-white md:justify-center",
+          staffCallOptions.length > 5 && "py-8 md:justify-start"
+        )}
+      >
         <div className="flex w-full flex-col gap-8 px-5 pt-6 md:w-95 md:px-0 md:pt-0 lg:w-120">
           <h2 className="text-gray-0 text-lg font-semibold md:hidden md:text-2xl lg:block">설정</h2>
           <div className="flex flex-col gap-6">
@@ -91,7 +89,7 @@ function MainSettingsPage() {
               </div>
             </SettingsSection>
             <SettingsSection title="기기">
-              <p className="text-sm font-medium text-gray-100">매장 기기 번호</p>
+              <p className="text-sm font-medium text-gray-100">KSNET 단말기 번호</p>
               <div className="flex items-center gap-1.5">
                 <Input
                   placeholder="기기 번호를 입력해주세요"
@@ -112,7 +110,7 @@ function MainSettingsPage() {
                       className: "h-8! w-fit! px-4! rounded-lg! text-s!",
                     },
                   }}
-                  onClick={handleUpdateDeviceNumber}
+                  onClick={handleRegisterDeviceNumber}
                 >
                   등록
                 </Button>
@@ -125,38 +123,45 @@ function MainSettingsPage() {
               )}
             </SettingsSection>
             <SettingsSection title="주문">
-              <div className="flex items-center justify-between">
-                <label htmlFor="customer-table-menu-popup" className="text-sm font-normal">
-                  손님 테이블 메뉴 팝업창 띄우기
-                </label>
-                <Switch
-                  id="customer-table-menu-popup"
-                  checked={showMenuPopup}
-                  onCheckedChange={(checked) => form.setValue("showMenuPopup", checked)}
-                />
-              </div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="customer-table-order-total-price" className="text-sm font-normal">
-                  손님 테이블 주문 내역에서 총 주문금액 표시하기
-                </label>
-                <Switch
-                  id="customer-table-order-total-price"
-                  checked={showOrderTotalPrice}
-                  onCheckedChange={(checked) => form.setValue("showOrderTotalPrice", checked)}
-                />
-              </div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="hall-order-menu-image"
-                  className="flex items-end gap-2.5 text-sm font-normal"
-                >
-                  홀 주문 내역에서 메뉴 이미지 표시하기
-                </label>
-                <Switch
-                  id="hall-order-menu-image"
-                  checked={showOrderMenuImage}
-                  onCheckedChange={(checked) => form.setValue("showOrderMenuImage", checked)}
-                />
+              <SettingsSwitchItem propName="showMenuPopup" label="손님 테이블 메뉴 팝업창 띄우기" />
+              <SettingsSwitchItem
+                propName="showOrderTotalPrice"
+                label="손님 테이블 주문 내역에서 총 주문금액 표시하기"
+              />
+              <SettingsSwitchItem
+                propName="showOrderMenuImage"
+                label="홀 주문 내역에서 메뉴 이미지 표시하기"
+              />
+              <div className="flex flex-col gap-3">
+                <Label className="flex items-end gap-2.5 text-sm font-normal">
+                  추가 테이블 수 설정
+                </Label>
+                <div className="flex items-center gap-1.5">
+                  <Input
+                    type="number"
+                    placeholder="테이블 수를 입력해주세요"
+                    className="h-8! rounded-[10px]! py-1.5! text-xs! lg:h-9!"
+                    {...form.register("extraTableCount")}
+                  />
+                  <Button
+                    color="black"
+                    responsive
+                    responsiveButtons={{
+                      lg: { buttonSize: "sm", className: "text-s! gap-0!" },
+                      md: {
+                        buttonSize: "custom",
+                        className: "h-8! w-fit! px-4! rounded-lg! text-s! ",
+                      },
+                      sm: {
+                        buttonSize: "custom",
+                        className: "h-8! w-fit! px-4! rounded-lg! text-s!",
+                      },
+                    }}
+                    onClick={handleRegisterDeviceNumber}
+                  >
+                    등록
+                  </Button>
+                </div>
               </div>
               <div className="flex flex-col gap-3">
                 <label
