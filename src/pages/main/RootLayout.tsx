69a1dc56fe3727f2a2df79333dc72a5fe2e9ff98
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2026-01-12 01:17:24 +0900

    fix: 루트 레이아웃 수정 및 isGuest 기본 조건을 pathname으로 지정

--- src/pages/main/RootLayout.tsx
+++ src/pages/main/RootLayout.tsx
@@ -1,31 +1,18 @@
-import { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import logoTextHorizontal from "@/assets/images/logo-text-horizontal.svg";
 import MobileSidebar from "@/components/layout/MobileSidebar";
 import SectionTitle from "@/components/layout/SectionTitle";
 import Sidebar from "@/components/layout/Sidebar";
 
 function RootLayout() {
-  const [isUser] = useState(true);
+  const location = useLocation();
+  const isGuest = location.pathname.startsWith("/guest");
 
   // TODO: 로그인 여부 확인 후 리다이렉트
 
   return (
     <MobileSidebar>
-      {isUser ? (
-        // 승인된 매장이 있을 경우
-        <div className="flex h-full w-dvw flex-col bg-gray-700 md:h-dvh md:flex-row md:gap-2.5 md:px-5 md:py-5 lg:gap-6 lg:px-15 lg:py-8">
-          <aside className="hidden h-auto flex-col rounded-[28px] bg-white md:flex md:w-46.5 md:gap-3 lg:w-79.5 lg:gap-4">
-            <Sidebar />
-          </aside>
-          <article className="flex h-full flex-1 flex-col bg-white md:rounded-[28px] md:p-5 lg:p-8">
-            <SectionTitle />
-            <div className="flex flex-1 flex-col overflow-y-auto">
-              <Outlet />
-            </div>
-          </article>
-        </div>
-      ) : (
+      {isGuest ? (
         // 승인된 매장이 없을 경우
         <div className="flex h-full w-full flex-col bg-gray-700">
           <header className="hidden flex-col md:flex md:gap-4 md:px-6 md:pt-3 lg:gap-6 lg:px-15 lg:pt-10">
@@ -36,6 +23,19 @@ function RootLayout() {
           <main className="center flex min-h-0 flex-1">
             <Outlet />
           </main>
+        </div>
+      ) : (
+        // 승인된 매장이 있을 경우
+        <div className="flex h-full w-dvw flex-col overflow-hidden bg-gray-700 md:h-dvh md:flex-row md:gap-2.5 md:px-5 md:py-5 lg:gap-6 lg:px-15 lg:py-8">
+          <aside className="hidden h-auto shrink-0 flex-col rounded-[28px] bg-white md:flex md:w-46.5 md:min-w-46.5 md:gap-3 lg:w-79.5 lg:min-w-79.5 lg:gap-4">
+            <Sidebar />
+          </aside>
+          <article className="flex h-full min-w-0 flex-1 flex-col bg-white md:rounded-[28px] md:p-5 lg:p-8">
+            <SectionTitle />
+            <div className="hide-scrollbar flex h-full flex-1 flex-col overflow-y-auto">
+              <Outlet />
+            </div>
+          </article>
         </div>
       )}
     </MobileSidebar>
