ce0ee1723f2efe8880d2eb0eb994af3618d7bcc4
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2026-01-11 17:57:33 +0900

    feat: 매장 신청 현황 UI 구현

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+import ApplicationTable from "@/components/pages/ApplicationTable";
+import Pagination from "@/components/ui/Pagination/Pagination";
+import { storeApplicationListMock } from "@/pages/main/guest/application/mock";
+
 function HomePage() {
-  return <div>Index</div>;
+  const [page, setPage] = useState(1);
+
+  // TODO: 매장 등록 신청 api 연동
+  // TODO: 페이지네이션 구현 (md:6, lg:10)
+
+  return (
+    <div className="flex h-full flex-col py-6">
+      <div className="flex-1 overflow-y-auto px-5 md:px-0">
+        <ApplicationTable type="user" applicationData={storeApplicationListMock.content} />
+      </div>
+      <div className="mt-auto flex justify-center pt-4">
+        <Pagination
+          currentPage={page}
+          pagination={storeApplicationListMock}
+          onPageChange={setPage}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
