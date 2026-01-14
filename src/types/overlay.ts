8c641da5e4505c7e2142b985272a0b6fd42d4df7
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2026-01-14 22:17:01 +0900

    fix: 추가했던 Alert Props 삭제

--- src/types/overlay.ts
+++ src/types/overlay.ts
@@ -2,9 +2,3 @@ export interface ModalProps {
   isOpen: boolean;
   close: () => void;
 }
-
-export interface AlertProps {
-  close: () => void;
-  onConfirm: () => void;
-  onCancel: () => void;
-}
