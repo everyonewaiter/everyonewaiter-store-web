32784b360589f5f92444c31e7a122c611895f420
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2026-01-14 18:13:41 +0900

    fix: button type 수정 submit -> button

--- src/pages/main/owner/devices/DeviceDetailModal.tsx
+++ src/pages/main/owner/devices/DeviceDetailModal.tsx
@@ -67,14 +67,7 @@ function DeviceDetailModal({ deviceId, ...props }: Readonly<DeviceDetailModalPro
     ];
   }, []);
 
-  const handleSave = (data: {
-    name?: string;
-    purpose?: DevicePurpose;
-    paymentType?: DevicePaymentType;
-    state?: string;
-    createdAt?: string;
-    tableNo?: number;
-  }) => {
+  const handleSubmit = () => {
     if (isEditing) {
       console.log(data);
       // TODO: 기기 정보 수정 로직
@@ -86,97 +79,93 @@ function DeviceDetailModal({ deviceId, ...props }: Readonly<DeviceDetailModalPro
 
   return (
     <Form {...form}>
-      <form id="device-detail-form" onSubmit={form.handleSubmit(handleSave)}>
-        <Modal
-          title="기기 정보"
-          hasCloseIcon={false}
-          open={props.isOpen}
-          onOpenChange={(open) => !open && props.close()}
-          footerContent={{
-            action: (
-              <Button
-                type="submit"
-                form="device-detail-form"
-                color={isEditing ? "primary" : "black"}
-                responsive
-                responsiveButtons={{
-                  md: { buttonSize: "md", className: "h-10! w-full" },
-                  lg: { buttonSize: "lg", className: "w-full" },
-                  sm: { buttonSize: "sm", className: "w-full" },
-                }}
-              >
-                {isEditing ? "저장하기" : "수정하기"}
-              </Button>
-            ),
-            cancel: (
-              <Button
-                color="grey"
-                responsive
-                responsiveButtons={{
-                  md: { buttonSize: "md", className: "h-10! w-30" },
-                  lg: { buttonSize: "lg", className: "w-40.5" },
-                  sm: { buttonSize: "sm", className: "w-18" },
-                }}
-                onClick={props.close}
-              >
-                닫기
-              </Button>
-            ),
-          }}
-        >
-          <div className="flex flex-col gap-3 lg:gap-4">
-            <FormField control={form.control} name="name" label="기기 이름" disabled={!isEditing} />
-            <FormField control={form.control} name="createdAt" label="등록일시" disabled />
-            <FormField
-              control={form.control}
-              name="state"
-              label="상태"
-              disabled
-              inputProps={{ value: data?.state === "ACTIVE" ? "활성화" : "비활성화" }}
+      <Modal
+        title="기기 정보"
+        hasCloseIcon={false}
+        open={props.isOpen}
+        onOpenChange={(open) => !open && props.close()}
+        footerContent={{
+          action: (
+            <Button
+              form="device-detail-form"
+              color={isEditing ? "primary" : "black"}
+              responsive
+              responsiveButtons={{
+                md: { buttonSize: "md", className: "h-10! w-full" },
+                lg: { buttonSize: "lg", className: "w-full" },
+                sm: { buttonSize: "sm", className: "w-full" },
+              }}
+              onClick={handleSubmit}
+            >
+              {isEditing ? "저장하기" : "수정하기"}
+            </Button>
+          ),
+          cancel: (
+            <Button
+              color="grey"
+              responsive
+              responsiveButtons={{
+                md: { buttonSize: "md", className: "h-10! w-30" },
+                lg: { buttonSize: "lg", className: "w-40.5" },
+                sm: { buttonSize: "sm", className: "w-18" },
+              }}
+              onClick={props.close}
+            >
+              닫기
+            </Button>
+          ),
+        }}
+      >
+        <div className="flex flex-col gap-3 lg:gap-4">
+          <FormField control={form.control} name="name" label="기기 이름" disabled={!isEditing} />
+          <FormField control={form.control} name="createdAt" label="등록일시" disabled />
+          <FormField
+            control={form.control}
+            name="state"
+            label="상태"
+            disabled
+            inputProps={{ value: data?.state === "ACTIVE" ? "활성화" : "비활성화" }}
+          />
+          <div className="flex flex-col gap-2">
+            <Label disabled={!isEditing}>권한</Label>
+            <Dropdown
+              dropdownItems={purposeDropdownItems}
+              defaultText={PURPOSE_TRANSLATES[purpose as DevicePurpose]}
+              value={purpose}
+              onChange={(value) => form.setValue("paymentType", value.id as DevicePaymentType)}
+              disabled={!isEditing}
             />
-            <div className="flex flex-col gap-2">
-              <Label disabled={!isEditing}>권한</Label>
-              <Dropdown
-                dropdownItems={purposeDropdownItems}
-                defaultText={PURPOSE_TRANSLATES[purpose as DevicePurpose]}
-                value={purpose}
-                onChange={(value) => form.setValue("paymentType", value.id as DevicePaymentType)}
-                disabled={!isEditing}
-              />
-            </div>
-            {purpose === "TABLE" && (
-              <>
-                <FormField
-                  control={form.control}
-                  name="tableNo"
-                  label="테이블 번호"
-                  disabled={!isEditing}
-                  inputProps={{
-                    type: "number",
-                    onChange: (e: { target: { value: string } }) => {
-                      const value = Number(e.target.value.replaceAll(/\D/g, ""));
-                      form.setValue("tableNo", value, { shouldValidate: true });
-                    },
-                  }}
-                />
-                <div className="flex flex-col gap-2">
-                  <Label disabled={!isEditing}>결제 방식</Label>
-                  <Dropdown
-                    dropdownItems={paymentTypeDropdownItems}
-                    // TODO: 데이터의 결제 방식에 맞게 수정
-                    defaultText={paymentTypeDropdownItems[0].name}
-                    value={paymentType}
-                    onChange={(value) =>
-                      form.setValue("paymentType", value.id as DevicePaymentType)
-                    }
-                    disabled={!isEditing}
-                  />
-                </div>
-              </>
-            )}
           </div>
-        </Modal>
-      </form>
+          {purpose === "TABLE" && (
+            <>
+              <FormField
+                control={form.control}
+                name="tableNo"
+                label="테이블 번호"
+                disabled={!isEditing}
+                inputProps={{
+                  type: "number",
+                  onChange: (e: { target: { value: string } }) => {
+                    const value = Number(e.target.value.replaceAll(/\D/g, ""));
+                    form.setValue("tableNo", value, { shouldValidate: true });
+                  },
+                }}
+              />
+              <div className="flex flex-col gap-2">
+                <Label disabled={!isEditing}>결제 방식</Label>
+                <Dropdown
+                  dropdownItems={paymentTypeDropdownItems}
+                  // TODO: 데이터의 결제 방식에 맞게 수정
+                  defaultText={paymentTypeDropdownItems[0].name}
+                  value={paymentType}
+                  onChange={(value) => form.setValue("paymentType", value.id as DevicePaymentType)}
+                  disabled={!isEditing}
+                />
+              </div>
+            </>
+          )}
+        </div>
+      </Modal>
     </Form>
   );
 }
