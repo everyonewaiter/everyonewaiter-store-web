5ab5b475aa3286a39da4f727cd3af643e390e83b
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2026-01-16 00:14:38 +0900

    fix: 카테고리 모달 순서 변경 적용 및 css 수정

--- src/pages/main/owner/menus/CategoryModal.tsx
+++ src/pages/main/owner/menus/CategoryModal.tsx
@@ -1,49 +1,70 @@
-import { useState } from "react";
-import { useForm, useWatch } from "react-hook-form";
+import { useEffect, useState } from "react";
+import { useFieldArray, useForm } from "react-hook-form";
 import { Form } from "@/components/form/Form";
 import FormField from "@/components/form/FormField";
 import { DragDrop, Plus, Trash, UpsideDown } from "@/components/icons";
 import Modal from "@/components/overlay/Modal";
 import Button from "@/components/ui/Button/Button";
+import { DragList } from "@/components/ui/Drag/DragList";
 import cn from "@/lib/utils";
-import { CATEGORIES_MOCK } from "@/pages/main/owner/menus/mock";
+import type { Category } from "@/types/domain/menu";
 import type { ModalProps } from "@/types/overlay";
 
-function CategoryModal({ isOpen, close }: Readonly<ModalProps>) {
+interface CategoryModalProps extends ModalProps {
+  categories: Category[];
+  onSave?: (categories: Category[]) => void;
+}
+
+function CategoryModal({ isOpen, close, categories, onSave }: Readonly<CategoryModalProps>) {
   const form = useForm({
     defaultValues: {
-      categories:
-        CATEGORIES_MOCK.map((category) => ({
-          id: category.categoryId,
-          name: category.name,
-        })) ?? [],
+      categories,
     },
   });
+
+  const { fields, append, remove } = useFieldArray({
+    control: form.control,
+    name: "categories",
+  });
+
+  useEffect(() => {
+    form.reset({ categories });
+  }, [categories, form]);
 
   const [mode, setMode] = useState<"CREATE" | "CHANGE_ORDER">("CREATE");
   const isCreating = mode === "CREATE";
   const isChangingOrder = mode === "CHANGE_ORDER";
 
-  const categories = useWatch({ control: form.control, name: "categories" });
+  const [changeOrdersList, setChangeOrdersList] = useState<
+    { sourceId: string; targetId: string; where: "PREV" | "NEXT" }[]
+  >([]);
 
-  const handleAddCategory = () => {
-    form.setValue("categories", [...categories, { id: String(categories.length + 1), name: "" }]);
+  const handleAddCategory = () => append({ categoryId: String(fields.length + 1), name: "" });
+  const handleDeleteCategory = (index: number) => remove(index);
+
+  const handleSave = () => {
+    // TODO: 카테고리 저장 API 호출
+
+    onSave?.(form.getValues().categories);
+    close();
   };
 
-  const handleDeleteCategory = (index: number) => {
-    form.setValue(
-      "categories",
-      categories.filter((_, i) => i !== index)
-    );
+  const handleSaveChanges = async () => {
+    changeOrdersList.forEach(() => {
+      // TODO: 카테고리 순서 변경 API 호출 (sourceId, targetId, where)
+    });
+
+    onSave?.(form.getValues().categories);
+    setMode("CREATE");
+    setChangeOrdersList([]);
   };
 
-  /**
-   *
-   * @param index - 카테고리 인덱스
-   */
-  const handleChangeOrderCategory = () => {
-    // TODO: 카테고리 순서 변경 로직 구현
-    setMode("CHANGE_ORDER");
+  const handleResetChanges = () => {
+    // TODO: 카테고리 순서 변경 초기화 로직 구현
+
+    form.reset({ categories });
+    setMode("CREATE");
+    setChangeOrdersList([]);
   };
 
   return (
@@ -62,6 +83,7 @@ function CategoryModal({ isOpen, close }: Readonly<ModalProps>) {
                 md: { buttonSize: "sm", className: "w-full" },
                 sm: { buttonSize: "sm", className: "w-full" },
               }}
+              onClick={isCreating ? handleSave : handleSaveChanges}
             >
               {isCreating ? "저장하기" : "순서 저장하기"}
             </Button>
@@ -71,11 +93,11 @@ function CategoryModal({ isOpen, close }: Readonly<ModalProps>) {
               color="grey"
               responsive
               responsiveButtons={{
-                lg: { buttonSize: "xl", className: "w-30 outline-none" },
+                lg: { buttonSize: "xl", className: "w-30" },
                 md: { buttonSize: "sm", className: "w-25" },
                 sm: { buttonSize: "sm", className: "w-25" },
               }}
-              onClick={() => (isCreating ? close() : setMode("CREATE"))}
+              onClick={isCreating ? close : handleResetChanges}
             >
               {isCreating ? "닫기" : "취소"}
             </Button>
@@ -97,40 +119,49 @@ function CategoryModal({ isOpen, close }: Readonly<ModalProps>) {
       >
         <div className="flex flex-1 flex-col overflow-hidden">
           <div className="hide-scrollbar flex flex-1 flex-col gap-3 overflow-y-auto pb-4 md:gap-3 md:pb-0">
-            {categories.map((category, index) => (
-              <FormField
-                key={category.id}
-                label={`카테고리 ${index + 1}`}
-                inputProps={{
-                  placeholder: "카테고리를 입력해주세요",
-                  readOnly: isChangingOrder,
-                  className: isChangingOrder ? "cursor-default" : "",
-                }}
-                {...form.register(`categories.${index}.name`)}
-                postfix={
-                  <Button
-                    variant="outline"
-                    className={cn(
-                      "center size-7 rounded-lg md:size-7 lg:size-10 lg:rounded-xl",
-                      isCreating ? "border-status-error" : "border-gray-600"
-                    )}
-                    onClick={() => {
-                      if (mode === "CREATE") {
-                        handleDeleteCategory(index);
-                      } else {
-                        handleChangeOrderCategory();
-                      }
-                    }}
-                  >
-                    {isCreating ? (
-                      <Trash className="text-status-error size-4 lg:size-6" />
-                    ) : (
-                      <DragDrop className="size-4 text-gray-100 lg:size-8" />
-                    )}
-                  </Button>
-                }
-              />
-            ))}
+            <DragList
+              items={fields}
+              onReorder={(items, sourceId, targetId, where) => {
+                form.setValue("categories", items);
+                setChangeOrdersList((prev) => [...prev, { sourceId, targetId, where }]);
+              }}
+              canDrag={mode === "CHANGE_ORDER"}
+              keyExtractor={(item) => item.categoryId}
+              renderItem={(category, index) => (
+                <FormField
+                  key={category.categoryId}
+                  name={`categories.${index}.name`}
+                  control={form.control}
+                  label={`카테고리 ${index + 1}`}
+                  inputProps={{
+                    placeholder: "카테고리를 입력해주세요",
+                    readOnly: isChangingOrder,
+                    className: isChangingOrder ? "cursor-default" : "",
+                  }}
+                  postfix={
+                    <Button
+                      variant="outline"
+                      className={cn(
+                        "center size-7 rounded-lg md:size-7 lg:size-10 lg:rounded-xl",
+                        isCreating ? "border-status-error" : "border-gray-600"
+                      )}
+                      onClick={() => {
+                        if (mode === "CREATE") {
+                          handleDeleteCategory(index);
+                        }
+                      }}
+                    >
+                      {isCreating ? (
+                        <Trash className="text-status-error size-4 lg:size-6" />
+                      ) : (
+                        <DragDrop className="size-4 text-gray-100 lg:size-8" />
+                      )}
+                    </Button>
+                  }
+                />
+              )}
+              className="space-y-2"
+            />
           </div>
           {isCreating && (
             <Button
