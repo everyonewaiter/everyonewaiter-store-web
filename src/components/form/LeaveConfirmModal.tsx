4a7ed8127fba89c28ab3fdbadf2fcdfba79136f9
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2026-01-16 12:19:06 +0900

    fix: 폼 이탈 모달 css 수정

--- src/components/form/LeaveConfirmModal.tsx
+++ src/components/form/LeaveConfirmModal.tsx
@@ -27,22 +27,23 @@ function LeaveConfirmModal({ isOpen, onCancel, onConfirm }: Readonly<LeaveConfir
   return createPortal(
     <Alert
       onClose={onCancel}
+      layoutClassName="w-80! md:w-100!"
       footer={
         <Alert.Footer>
-          <Alert.Cancel color="grey" onClick={onCancel} className="button-lg">
+          <Alert.Cancel color="grey" onClick={onCancel} className="button-sm lg:button-lg">
             취소
           </Alert.Cancel>
-          <Alert.Action color="black" onClick={onConfirm} className="button-lg">
+          <Alert.Action color="black" onClick={onConfirm} className="button-sm lg:button-lg">
             나가기
           </Alert.Action>
         </Alert.Footer>
       }
     >
-      <div className="flex flex-col gap-2 py-3">
-        <span className="text-primary text-xl font-semibold">
+      <div className="flex w-full flex-col gap-2 py-3">
+        <span className="text-primary text-md font-semibold lg:text-xl">
           현재 저장되지 않은 폼 내역이 있습니다.
         </span>
-        <span className="text-gray-0 text-lg font-medium">
+        <span className="text-gray-0 text-sm font-medium lg:text-lg">
           저장하지 않고 목록으로 이동하시겠습니까?
         </span>
       </div>
