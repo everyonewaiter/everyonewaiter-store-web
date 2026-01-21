e85b18d027b48c3bfb098e631fe491a4b9cd31f5
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2026-01-21 19:02:40 +0900

    chore: cypress config 작성

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    pageLoadTimeout: 200000,
     setupNodeEvents() {
       // implement node event listeners here
     },
