395fc7f5034f12a037cdcc4586594472ae460689
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2026-01-26 22:58:24 +0900

    fix: vite.config 수정 (vitest 삭제)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
-import { defineConfig } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -19,9 +19,5 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
-  },
-  test: {
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.ts",
   },
 });
