9fd2a7fad4cc58adb864dce2a413ec630d51485b
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2026-01-28 16:01:19 +0900

    feat: account queries에 select 추가, 섹션 타이틀 유저 정보에 이메일 연결

--- src/components/layout/SectionTitle.tsx
+++ src/components/layout/SectionTitle.tsx
@@ -1,5 +1,7 @@
 import { useRef, useState } from "react";
+import { useQuery } from '@tanstack/react-query';
 import { useLocation, useNavigate } from "react-router-dom";
+import { accountQueries } from '@/api/account/queries';
 import { User } from "@/components/icons";
 import useOutsideClick from "@/hooks/useOutSideClick";
 
@@ -8,6 +10,8 @@ function SectionTitle() {
   const navigate = useNavigate();
   const buttonRef = useRef<HTMLButtonElement>(null);
   const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const { data: account } = useQuery(accountQueries.getMe())
 
   const [isOpenUser, setIsOpenUser] = useState(false);
 
@@ -33,14 +37,13 @@ function SectionTitle() {
 
   if (!getText()) return null;
 
-  const email = "asdf@gmail.com";
-
   const handleLogout = () => {
     localStorage.removeItem("storeId");
     localStorage.removeItem("token");
     navigate("/login");
     setIsOpenUser(false);
   };
+
 
   return (
     <div className="relative hidden flex-col md:flex md:gap-2 lg:gap-5">
@@ -56,18 +59,18 @@ function SectionTitle() {
         </button>
         {isOpenUser && (
           <div className="absolute right-0 z-9999 flex flex-col gap-1 rounded-2xl bg-white p-3 shadow-[0px_2px_10px_0px_rgba(0,0,0,0.08)] md:top-9 lg:top-14">
-            <div className="flex items-center gap-2 rounded-xl bg-gray-700 p-3">
+            <div className="flex items-center gap-2 rounded-lg lg:rounded-xl bg-gray-700 p-2 lg:p-3">
               <div className="center h-5.5 w-5.5 rounded-full border border-gray-500 bg-white">
                 <User className="size-4 text-gray-400" />
               </div>
-              <span className="text-[15px] text-gray-100">{email}</span>
+              <span className="text-s lg:text-[15px] text-gray-100">{account?.email}</span>
             </div>
             <button
               type="button"
               className="flex items-center gap-2 rounded-xl p-3"
               onClick={handleLogout}
             >
-              <span className="text-[15px] text-gray-300">로그아웃</span>
+              <span className="text-s lg:text-[15px] text-gray-300">로그아웃</span>
             </button>
           </div>
         )}
